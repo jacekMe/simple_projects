a35a860062264f5c24405a17cd536d31c75ce9f1
Jacek Meres <jacekmeres@gmail.com>
2022-06-12 22:21:25 +0200

    day of birth

---
+++ 01_day_of_birth.py
@@ -0,0 +1,40 @@
+import datetime
+import calendar
+
+# def translate_to_polish(day_name):
+#     match day_name:
+#         case 'Monday':
+#             return 'Poniedziałek'
+#         case 'Tuesday':
+#             return 'Wtorek'
+#         case 'Wednesday':
+#             return 'Środa'
+#         case 'Thursday':
+#             return 'Czwartek'
+#         case 'Friday':
+#             return 'Piątek'
+#         case 'Saturday':
+#             return 'Sobota'
+#         case 'Sunday':
+#             return 'Niedziela'
+
+def translate_to_polish_v2(day_name):
+    english_to_polish_day_name = {
+        'Monday': 'Poniedziałek',
+        'Tuesday': 'Wtorek',
+        'Wednesday': 'Środa',
+        'Thursday': 'Czwartek',
+        'Friday': 'Piątek',
+        'Saturday': 'Sobota',
+        'Sunday': 'Niedziela'
+    }
+
+    return english_to_polish_day_name[day_name]
+date_of_birth = input("Podaj datę urodzin (dd-mm-yyyy): ")
+day, month, year = date_of_birth.split("-")
+
+date_of_birth = datetime.datetime(int(year), int(month), int(day))
+
+day_name = calendar.day_name[date_of_birth.weekday()]
+print(day_name)
+print(translate_to_polish_v2(day_name))
